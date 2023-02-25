8d116ca7621aedb9adb4f0ce9954c4952dc38b63
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-02-26 00:16:13 +0900

    データ読込時のローディング設定をしよう

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utiles/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">{loading ? <p>Loading...</p> : <p>Loaded</p>}</div>
+  );
 }
 
 export default App;
